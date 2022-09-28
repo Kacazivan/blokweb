08d87292d2c9c2e868cef0240209a6174a542d3f
Kacazivan <112778173+Kacazivan@users.noreply.github.com>
2022-09-28 11:36:45 +0200

    Thema switcher
    Thema switcher erin verwerkt, alleen erin gezet. Nog niet helemaal bewerkt

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,15 @@ console.log("hi");
 
 var toggleButton = document.getElementsByClassName('toggle-button')[0]
 var navbarLinks = document.getElementsByClassName('navbar-links')[0]
+var themaSwitcherButton = document.querySelector('footer button');
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
+
+function themaSwitcher(){
+    console.log('halloween')
+    document.documentElement.classList.toggle('halloween');
+}
+
+themaSwitcherButton.addEventListener('click', themaSwitcher);
