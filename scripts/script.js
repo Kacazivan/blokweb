5d8a1bc783558a6006aa8337c09d93b932d9e9cb
Kacazivan <112778173+Kacazivan@users.noreply.github.com>
2022-09-29 13:51:28 +0200

    Site geupdatet
    de halloween thema er meer in verwerkt

--- scripts/script.js
+++ scripts/script.js
@@ -9,9 +9,24 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
 
+
 function themaSwitcher(){
     console.log('halloween')
     document.documentElement.classList.toggle('halloween');
-}
+    changeImage();
+};
 
 themaSwitcherButton.addEventListener('click', themaSwitcher);
+
+
+//https://www.youtube.com/watch?v=1j-XMwrLcg8 --> dit filmpje heeft me geholpen
+function changeImage(){
+    console.log('change logo')
+    var displayImage = document.getElementById('origineelLogo')
+
+    if (displayImage.src.match('images/logoHalloween.png')){
+        displayImage.src = 'images/logoHalloweenWit.png'
+    }else{
+        displayImage.src = 'images/logoHalloween.png'
+    }
+};
